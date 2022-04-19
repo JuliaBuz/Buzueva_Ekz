9710a61a6081732b83e38bd12784ef6d3a808732
JuliaBuz <y.buzueva2002@mail.ru>
2022-04-19 11:20:30 +0300

    Финальная версия

--- Buzueva_Ekzamen/Pages/Catalog.xaml.cs
+++ Buzueva_Ekzamen/Pages/Catalog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,15 +24,17 @@ namespace Buzueva_Ekzamen.Pages
         public int CountTakedBooks { get; set; }
         List<int> korzina = new List<int>();
         Otobrazhenie viewModel = new Otobrazhenie();
-        int totalcost = 0;
+        int TotalCost = 0;
         int TotalSale = 0;
         public List<BooksCatalog> buf3 = new List<BooksCatalog>();
         public BooksCatalog buf4;
+        public string skid { get; set; }
         public Catalog()
         {
             InitializeComponent();
             Allbooks.ItemsSource = viewModel.catolog.ToList();
             Allbooks.Items.Refresh();
+            skid = "None";
         }
 
         private void add_Click(object sender, RoutedEventArgs e)
@@ -43,33 +46,30 @@ namespace Buzueva_Ekzamen.Pages
                 Button button = (Button)sender;
                 int id = Convert.ToInt32(button.Uid);
                 BooksCatalog books = DataBase.BaseModel.BooksCatalog.FirstOrDefault(x => x.ID == id);
-
                 int ss = (int)(books.NumberStok + books.NumberMagazine);
-
                 if (books.NumberStok == 0 && books.NumberMagazine == 0)
                 {
                     throw new Exception("Данной книги нет в наличии.");
                 }
-
                 books.CountInCart++;
                 if (books.CountInCart > ss)
                 {
                     throw new Exception("Превышен лимит товара");
                 }
-
                 korzina.Add(id);
                 CountTakedBooks = korzina.Count;
-
-                totalcost += Convert.ToInt32(books.Price);
-
+                TotalCost += Convert.ToInt32(books.Price);
                 Class1 class1 = new Class1();
-                TotalSale = class1.sale(korzina.Count, totalcost);
-
+                TotalSale = class1.sale(korzina.Count, TotalCost);
                 shtuk.Text = korzina.Count.ToString();
-                price.Text = totalcost.ToString();
+                price.Text = TotalCost.ToString();
                 sale.Text = TotalSale.ToString();
-
-                double itog = totalcost - (totalcost * TotalSale / 100);
+                if (TotalSale > 0)
+                {
+                    price.TextDecorations = TextDecorations.Strikethrough;
+                    newprice.Visibility = Visibility.Visible;
+                }
+                double itog = TotalCost - (TotalCost * TotalSale / 100);
                 newprice.Text = Math.Floor(itog).ToString();
             }
             catch (Exception ex)
@@ -88,6 +88,7 @@ namespace Buzueva_Ekzamen.Pages
                     buf4 = DataBase.BaseModel.BooksCatalog.FirstOrDefault(x => x.ID == y);
                     buf3.Add(buf4);
                 }
+                PagesChange.switchPage.Navigate(new Korzina(buf3, TotalSale));
             }
 
             catch (Exception ex)
---
+++ Buzueva_Ekzamen/Pages/Korzina.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Buzueva_Ekzamen.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Korzina.xaml
+    /// </summary>
+    public partial class Korzina : Page, INotifyPropertyChanged
+    {
+        List<BooksCatalog> bo = new List<BooksCatalog>();
+        public event PropertyChangedEventHandler PropertyChanged;
+        int Sale;
+        Otobrazhenie ViewModel = new Otobrazhenie();
+        int c = 0, tc = 0, itogc = 0, itogtc = 0; bool d = true;
+        public Korzina(List<BooksCatalog> books, int sale)
+        {
+            InitializeComponent();
+            ListZak.ItemsSource = books;
+            ListZak.Items.Refresh();
+            Sale = sale;
+            bo = books;
+            foreach (BooksCatalog b in books)
+            {
+
+                if (b.CountInCart == 1)
+                {
+                    b.Visible = "Collapsed";
+                }
+                if (sale == 0)
+                {
+                    b.Visible2 = "Collapsed";
+                }
+                b.TotalPriseZak = (decimal)(b.Price * b.CountInCart);
+                b.TotalPriseZakWithSale = b.TotalPriseZak - (b.TotalPriseZak * sale / 100);
+                b.decor = "Strikethrough";
+            }
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            PagesChange.switchPage.Navigate(new Catalog());
+        }
+
+        private void AllDelit(object sender, RoutedEventArgs e)
+        {
+            ListZak.ItemsSource = null;
+            ListZak.Items.Refresh();
+        }
+
+        private void Delit(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int id = Convert.ToInt32(button.Uid);
+            BooksCatalog prov = DataBase.BaseModel.BooksCatalog.FirstOrDefault(x => x.ID == id);
+            bo.Remove(prov);
+            ListZak.Items.Refresh();
+        }
+        private void NewZakaz(object sender, RoutedEventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            foreach (BooksCatalog b in bo)
+            {
+                c = b.CountInCart;
+                tc = Convert.ToInt32(b.TotalPriseZakWithSale);
+                itogc += c;
+                itogtc += tc;
+            }
+            Zakaz zakaz = new Zakaz()
+            {
+                Date = today.AddHours(72),
+                Count = itogc,
+                TotalPrice = itogtc,
+                Sale = Sale,
+                Reserv = today.AddDays(7),
+            };
+            DataBase.BaseModel.Zakaz.Add(zakaz);
+            DataBase.BaseModel.SaveChanges();
+            DataBase.BaseModel = new Entities();
+
+            ListZak.ItemsSource = null;
+            ListZak.Items.Refresh();
+            MessageBox.Show("Следующие данные о заказе добавлены в БД:\n Номер заказа:" + zakaz.ID.ToString() + "\n Можно забирать:" + today.AddHours(72).ToString() +
+            "\n Конец срока резервирования продукции:" + today.AddDays(7).ToString() + "\n Цена заказа со скидкой:" + itogtc.ToString() + "\n Процент скидки:" + Sale.ToString() +
+            "\n Итоговое количество:" + itogc.ToString());
+        }
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (BooksCatalog b in bo)
+            {
+                b.CountInCart += 1;
+                if (b.CountInCart == 1)
+                {
+                    b.Visible = "Collapsed";
+                }
+                if (Sale == 0)
+                {
+                    b.Visible2 = "Collapsed";
+                }
+                b.TotalPriseZak = ((decimal)(b.Price * b.CountInCart));
+                b.TotalPriseZakWithSale = b.TotalPriseZak - (b.TotalPriseZak * Sale / 100);
+                b.decor = "Strikethrough";
+            }
+            ListZak.Items.Refresh();
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BooksCatalog prov = new BooksCatalog();
+                foreach (BooksCatalog b in bo)
+                {
+                    b.CountInCart -= 1;
+                    if ((b.CountInCart > 0) && (b.CountInCart < (b.NumberStok + b.NumberStok)))
+                    {
+                        if (b.CountInCart == 1)
+                        {
+                            b.Visible = "Collapsed";
+                        }
+                        if (Sale == 0)
+                        {
+                            b.Visible2 = "Collapsed";
+                        }
+                        b.TotalPriseZak = ((decimal)(b.Price * b.CountInCart));
+                        b.TotalPriseZakWithSale = b.TotalPriseZak - (b.TotalPriseZak * Sale / 100);
+                        b.decor = "Strikethrough";
+                        ListZak.Items.Refresh();
+                    }
+                    else if (b.CountInCart == 0)
+                    {
+                        prov = b;
+                    }
+                    else
+                    {
+                        throw new Exception("Количество не может быть");
+                    }
+                }
+                bo.Remove(prov);
+                ListZak.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+    }
+}
